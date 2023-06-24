b89d646cfd9cf5087f1f7c5bc7018c7271c1a7ac
phantom-mayavi <36674493+phantom-mayavi@users.noreply.github.com>
2023-06-24 14:13:06 -0400

    list and alert comp added

--- src/app/product-list/product-list.component.ts
+++ src/app/product-list/product-list.component.ts
@@ -5,7 +5,7 @@ import { products } from '../products';
 @Component({
   selector: 'app-product-list',
   templateUrl: './product-list.component.html',
-  styleUrls: ['./product-list.component.css']
+  styleUrls: ['./product-list.component.css'],
 })
 export class ProductListComponent {
   products = [...products];
@@ -14,7 +14,6 @@ export class ProductListComponent {
     window.alert('The product has been shared!');
   }
 }
-
 
 /*
 Copyright Google LLC. All Rights Reserved.
