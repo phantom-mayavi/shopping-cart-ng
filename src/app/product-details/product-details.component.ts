9739ffc180ea3f8d0f09fd2fbd294f95a597180e
phantom-mayavi <36674493+phantom-mayavi@users.noreply.github.com>
2023-06-28 06:57:50 -0400

    product details

--- src/app/product-details/product-details.component.ts
+++ src/app/product-details/product-details.component.ts
@@ -1,10 +1,23 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Product, products } from '../products';
 
 @Component({
   selector: 'app-product-details',
   templateUrl: './product-details.component.html',
-  styleUrls: ['./product-details.component.css']
+  styleUrls: ['./product-details.component.css'],
 })
 export class ProductDetailsComponent {
+  product: Product | undefined;
 
+  constructor(private route: ActivatedRoute) {}
+
+  ngOnInit() {
+    const routeParams = this.route.snapshot.paramMap;
+    const productIdFromRoute = Number(routeParams.get('productId'));
+
+    this.product = products.find(
+      (product) => product.id === productIdFromRoute
+    );
+  }
 }
